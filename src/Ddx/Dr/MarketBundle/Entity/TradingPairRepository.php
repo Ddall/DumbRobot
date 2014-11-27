7649cc08265d295a989706dacf4aa6a23e75dbc4
Allan IRDEL <allan.irdel@gmail.com>
2014-11-28 00:09:59 +0100

    TradingPairRepository: cleanup

--- src/Ddx/Dr/MarketBundle/Entity/TradingPairRepository.php
+++ src/Ddx/Dr/MarketBundle/Entity/TradingPairRepository.php
@@ -3,28 +3,13 @@
 namespace Ddx\Dr\MarketBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
-use Ddx\Dr\ReaderBundle\Market;
 use Ddx\Dr\MarketBundle\Entity\TradingPair;
+use Ddx\Dr\MarketBundle\Entity\Market;
+
 
 /**
  * TradeRepository
  */
 class TradingPairRepository extends EntityRepository
 {
-    /**
-     * @param Market $market
-     * @param TradingPair $pair
-     * @return Trade
-     */
-    public function getLastTrade(Market $market, TradingPair $pair){
-        return $this->createQueryBuilder('t')
-                ->select('t')
-                ->where('t.market_id = :market_id')
-                ->andWhere('t.tradingPair_id = !tp_id')
-                ->orderBy('t.id', 'DESC')
-                ->setParameter('market_id', $market->getId() )
-                ->setParameter('tp_id', $pair->getId())
-                ->getQuery()->getSingleResult()
-                ;
-    }
 }
