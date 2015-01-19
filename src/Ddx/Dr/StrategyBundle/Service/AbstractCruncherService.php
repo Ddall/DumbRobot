76a2471281414f0700b79cd6fee4d1af65f1db91
Allan IRDEL <allan.irdel@gmail.com>
2015-01-20 00:32:44 +0100

    Strategy entities

---
+++ src/Ddx/Dr/StrategyBundle/Service/AbstractCruncherService.php
@@ -0,0 +1,9 @@
+<?php
+namespace Ddx\Dr\StrategyBundle\Service;
+
+use Ddx\Dr\ReaderBundle\Service\AbstractDdxDrService;
+
+class AbstractCruncherService extends AbstractDdxDrService{
+    
+    
+}
