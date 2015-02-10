0317ada80c91e4cbe63e0cce697abe751a2e7e4c
Allan IRDEL <allan.irdel@gmail.com>
2015-02-10 01:07:39 +0100

    WIP EmaCruncher

--- src/Ddx/Dr/StrategyBundle/Service/Cruncher/EMACruncher.php
+++ src/Ddx/Dr/StrategyBundle/Service/Cruncher/EMACruncher.php
@@ -7,7 +7,30 @@ namespace Ddx\Dr\StrategyBundle\Service\Cruncher;
  * @author Allan
  */
 use Ddx\Dr\StrategyBundle\Service\AbstractCruncherService;
+use Ddx\Dr\ReaderBundle\Market;
+use Ddx\Dr\MarketBundle\Entity\TradingPair;
 
 class EMACruncher extends AbstractCruncherService{
+
+    const WORK_PERIOD = 1;
+
+    /**
+     * 
+     * @param Market $market
+     * @param TradingPair $tradingPair
+     * @param type $parameters
+     */
+    public function work(Market $market, TradingPair $tradingPair, $parameters = array() ){
+        
+    }
     
+    protected function handleParameters($parameters){
+        if(!is_array($parameters)){
+            throw new Exception('EmaCruncher::handleParameters wrong format');
+        }
+        
+        if(!key_exists(self::WORK_PERIOD, $parameters)){
+            $parameters[self::WORK_PERIOD] = null;
+        }
+    }
 }
