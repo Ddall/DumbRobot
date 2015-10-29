14d636d8b4f62a8c4f190cab6ec5301ce89ac9fe
Allan IRDEL <allan.irdel@gmail.com>
2015-10-29 22:43:25 +0100

    fix dead code

--- src/Ddx/Dr/WebBundle/Controller/IndexController.php
+++ src/Ddx/Dr/WebBundle/Controller/IndexController.php
@@ -33,17 +33,13 @@ class IndexController extends Controller{
         $vwap = $tradingRepo->getWeightedData($kraken, $kraken->getActiveTradingPairs()->first(), 300);
         
         $tmp = array();
-        die('<pre>' . print_r(gettype($allTrades->toArray()), true));
-//        
         foreach($allTrade->toArray()  as $trade){
-            die('lol');
-//            $tmp[] = array(
-//                'x' => $trade->getTimeRemote(),
-//                'y' => $trade->getVolume(),
-//            );
+            $tmp[] = array(
+                'x' => $trade->getTimeRemote(),
+                'y' => $trade->getVolume(),
+            );
         }
         
-die('lolilol');
         return $this->render('DdxDrWebBundle:History:index.html.twig', array(
             'allTrades' => $allTrades,
             'vwapData' => $vwap,
